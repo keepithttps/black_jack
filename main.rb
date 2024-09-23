a17bcb196292f43ead87381d76e448d55cf09c5b
Nikolay M <m911nn@gmail.com>
2024-09-23 21:17:11 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -27,44 +27,48 @@ item = 1
 loop do
   case item
   when 1
+    messenge("Карту!____________________________________")
     min.distribute("player")
     min.distribute("dealer")
     min.show_cards
     item += 1
+    sleep 1
   when 2
-    messenge("____________________________________")
+    messenge("Ещё карту!____________________________________")
     min.distribute("player")
     min.distribute("dealer")
     min.show_cards
-    sleep 2
     messenge("Делайте ставки Гаспода!")
     @player_bank.make_bet
     @dealer_bank.make_bet
-    sleep 3
-    messenge("Ставки сделаны!")
-    messenge("____________________________________")
     sleep 2
+    messenge("Ставки сделаны!")
+    sleep 2
+    messenge("____________________________________")
     show_balance
     min.player_choice
     item = gets.chomp.to_i
   when 3  # Пропустить_ход игроку
+    messenge("Вы пропускаете ход ____________________________________")
     min.distribute("dealer") if min.sum_cards_dealer < 17
     item += 2
   when 4  # Добавить_карту
-    min.distribute("player") if min.sum_cards_player < 17
+    messenge("Ещё карту!____________________________________")
+    min.distribute("player")
     min.distribute("dealer") if min.sum_cards_dealer < 17
     item += 1
   when 5  # открыть карты
+    messenge("Открываем карты____________________________________")
     min.open_cards
-    if min.who_won == 1
+    if min.who_won == '1'
       @player_bank.win_bet
       messenge("Победил #{name_player}!!!")
       messenge("Ваш баланс #{@player_bank.balance}")
-    elsif min.who_won == -1
+    elsif min.who_won == '-1'
       @dealer_bank.win_bet
       messenge("Победил Диллер!!")
       messenge("Баланс #{@dealer_bank.balance}")
-    elsif min.who_won == 0
+    elsif min.who_won == '0'
       @player_bank.return_bet
       @dealer_bank.return_bet
       messenge("Ничья")
@@ -74,12 +78,14 @@ loop do
     messenge("____________________________________")
     messenge("Для выхода из Казино Введите 'end' ")
     messenge("Хотите продолжить Игру? Введите '6' ")
+    show_balance
     item = gets.chomp.to_i
     break if @dealer_bank.balance == 0 || @player_bank.balance == 0 || item == "end".to_i
   when 6
+    messenge("____________________________________")
     min.delete_deck_cards
     item = 1
   end
 end
-  
+
 
